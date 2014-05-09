394846c2ff966d770e390a6df48b9435362ce7e9
Jonathan Frederic <jon.freder@gmail.com>
2014-05-09 12:29:52 -0500

    Added init and fixed js path

--- widgetmode/widgetmode.py
+++ widgetmode/widgetmode.py
@@ -2,5 +2,5 @@
 from IPython.display import display, Javascript
 
 def widgetmode():
-    with open('./widgetmode.js', 'r') as f:
+    with open('./static/widgetmode.js', 'r') as f:
         display(Javascript(data=f.read()))
